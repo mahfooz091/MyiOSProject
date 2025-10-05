a4a74428a0b35181557d57524377edf431981650
Mahfooz Alam <ma.mahfoozalam8789@gmail.com>
2025-10-05 23:05:09 +0530

    Initial Commit

---
+++ FindMyAge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FindMyAge
+//
+//  Created by Mahfooz0123 on 10/5/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ FindMyAgeTests/FindMyAgeTests.swift
@@ -0,0 +1,17 @@
+//
+//  FindMyAgeTests.swift
+//  FindMyAgeTests
+//
+//  Created by Mahfooz0123 on 10/5/25.
+//
+
+import Testing
+@testable import FindMyAge
+
+struct FindMyAgeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
